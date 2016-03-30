5d6eb863cc94d1d264b5ab30fa4756e5a795a72c
Deepank Agarwal <deepank411@gmail.com>
2016-03-30 22:39:04 +0530

    second commit, stable

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function get_selection() {
   return txt;
 }
 
-$(".content").dblclick(function(e) {
+$(".content").dblclick(function() {
   mytext = get_selection();
   if(mytext != ''){
     // alert("double click " + mytext);
@@ -23,6 +23,8 @@ $(".content").dblclick(function(e) {
     $(".editor-toolbar").css("display","none");
   }
 });
+
+// select text by horizontal selection via cursor
 $(".content").bind("mouseup", function(e) {
   mytext = get_selection();
   if(mytext != ''){
@@ -32,6 +34,7 @@ $(".content").bind("mouseup", function(e) {
     $(".editor-toolbar").css("display","none");
   }
 });
+
 $('.editor-action-bold').click(function(){
   document.execCommand('bold');
 });
@@ -43,4 +46,29 @@ $('.editor-action-underline').click(function(){
 });
 $('.editor-action-changecolor').click(function(){
   document.execCommand('foreColor', false,  'red');
+});
+
+$(".done").click(function(){
+  var temp = document.getElementById("content").outerHTML;
+  console.log(temp);
+  var content = temp.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+  console.log(content);
+  var re = /<a>(.*?)<\/a>/g;
+  var links = content.match(re); // regex to match all links of this format
+  console.log(links);
+
+  var ul = document.getElementById("links-display");
+  for(var i = 0; i< links.length; i++){ // populate ul with the links colored red and blue alternatively
+    var li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML = li.innerHTML + links[i];
+  }
+});
+
+$(".match").click(function(){
+  var content = document.getElementById("content").innerText;
+  console.log(content);
+  var re = /\b\w{4}\b/g; // regex to match all 4 letter words
+  res = content.match(re);
+  console.log(res);
 });
