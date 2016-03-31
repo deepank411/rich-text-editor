8ac5d61d0aee0bb8bc9ee03bf1e33fa47abf40a3
Deepank Agarwal <deepank411@gmail.com>
2016-04-01 01:52:42 +0530

    random words fetching from url

--- index.js
+++ index.js
@@ -1,98 +1,125 @@
+var counter = 0;
+// l = 10;
+arr = new Array();
+
+function doneFunc(){
+   console.log("words are: " + arr);
+   console.log("length of arr is: " + arr.length);
+}
+
+window.getData=function()
+{
+   $.ajax({
+      url:'http://randomword.setgetgo.com/get.php?len=4',
+      async: false,
+      dataType: 'jsonp',
+      success:function(data){
+         arr.push(data.Word);
+         $('.quoteList').append('<li>' + data.Word +'</li>');
+         if(arr.length == l)
+         doneFunc();
+      }
+   });
+}
+
 $(document).ready(function(){
-  $("p").addClass("paragraph");
+   $("p").addClass("paragraph");
 });
 
 function get_selection() {
-  var txt = '';
-  if (window.getSelection) {
-    console.log("");
-    txt = window.getSelection();
-    oRange = txt.getRangeAt(0); //get the text range
-    oRect = oRange.getBoundingClientRect(); // get coordinates of selection
-    // console.log(oRect);
-    // console.log(oRect.top);
-  }
-  // for cross browser compatibility
-  // else if (document.getSelection) {
-  //   console.log("doc");
-  //   txt = document.getSelection();
-  // }
-  // else if (document.selection) {
-  //   console.log("doc range");
-  //   txt = document.selection.createRange().text;
-  // }
-  return [txt, oRect];
+   var txt = '';
+   if (window.getSelection) {
+      console.log("");
+      txt = window.getSelection();
+      oRange = txt.getRangeAt(0); //get the text range
+      oRect = oRange.getBoundingClientRect(); // get coordinates of selection
+      // console.log(oRect);
+      // console.log(oRect.top);
+   }
+   // for cross browser compatibility
+   // else if (document.getSelection) {
+   //   console.log("doc");
+   //   txt = document.getSelection();
+   // }
+   // else if (document.selection) {
+   //   console.log("doc range");
+   //   txt = document.selection.createRange().text;
+   // }
+   return [txt, oRect];
 }
 
 function placeToolbar(x_pos, y_pos) {
-  var d = document.getElementById('editor-toolbar');
-  d.style.position = "absolute";
-  d.style.left = x_pos-80+'px';
-  d.style.top = y_pos-60+'px';
+   var d = document.getElementById('editor-toolbar');
+   d.style.position = "absolute";
+   d.style.left = x_pos + 'px';
+   d.style.top = y_pos - 30 +'px';
 }
 
 $(".content").dblclick(function() {
-  mytext = get_selection()[0];
-  coord = get_selection()[1];
-  console.log(coord);
-  if(mytext != ''){
-    // alert("double click " + mytext);
-    $(".editor-toolbar").css("display","block");
-    placeToolbar(coord.left, coord.top);
-  } else{
-    $(".editor-toolbar").css("display","none");
-  }
+   mytext = get_selection()[0];
+   coord = get_selection()[1];
+   console.log(coord);
+   if(mytext != ''){
+      // alert("double click " + mytext);
+      $(".editor-toolbar").css("display","block");
+      placeToolbar(coord.left, coord.top);
+   } else{
+      $(".editor-toolbar").css("display","none");
+   }
 });
 
 // select text by horizontal selection via cursor
 $(".content").bind("mouseup", function(e) {
-  mytext = get_selection()[0];
-  coord = get_selection()[1];
-  console.log(coord);
-  if(mytext != ''){
-    // alert("selection " + mytext);
-    $(".editor-toolbar").css("display","block");
-    placeToolbar(coord.left, coord.top);
-  } else{
-    $(".editor-toolbar").css("display","none");
-  }
+   mytext = get_selection()[0];
+   coord = get_selection()[1];
+   console.log(coord);
+   if(mytext != ''){
+      // alert("selection " + mytext);
+      $(".editor-toolbar").css("display","block");
+      placeToolbar(coord.left, coord.top);
+   } else{
+      $(".editor-toolbar").css("display","none");
+   }
 });
 
 $('.editor-action-bold').click(function(){
-  document.execCommand('bold');
+   document.execCommand('bold');
 });
 $('.editor-action-italic').click(function(){
-  document.execCommand('italic');
+   document.execCommand('italic');
 });
 $('.editor-action-underline').click(function(){
-  document.execCommand('underline');
+   document.execCommand('underline');
 });
 $('.editor-action-changecolor').click(function(){
-  document.execCommand('foreColor', false,  'red');
+   document.execCommand('foreColor', false,  'red');
 });
 
 $(".done").click(function(){
-  var temp = document.getElementById("content").outerHTML;
-  console.log(temp);
-  var content = temp.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
-  console.log(content);
-  var re = /<a>(.*?)<\/a>/g;
-  var links = content.match(re); // regex to match all links of <a>()</a> format
-  console.log(links);
+   var temp = document.getElementById("content").outerHTML;
+   var content = temp.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+   console.log(content);
+   var re = /<a>(.*?)<\/a>/g;
+   var links = content.match(re); // regex to match all links of <a>()</a> format
+   console.log(links);
 
-  var ul = document.getElementById("links-display");
-  ul.innerHTML = ""; // prevents repeated adding of li on multiple clicks on button
-  for(var i = 0; i< links.length; i++){ // populate ul with the links colored red and blue alternatively
-    var li = document.createElement("li");
-    ul.appendChild(li);
-    li.innerHTML = li.innerHTML + links[i];
-  }
+   var ul = document.getElementById("links-display");
+   ul.innerHTML = ""; // prevents repeated adding of li on multiple clicks on button
+   for(var i = 0; i< links.length; i++){ // populate ul with the links colored red and blue alternatively
+      var li = document.createElement("li");
+      ul.appendChild(li);
+      li.innerHTML = li.innerHTML + links[i];
+   }
 });
 
 $(".match").click(function(){
-  var content = document.getElementById("content").innerText;
-  console.log(content);
-  var re = /\b\w{4}\b/g; // regex to find all 4 letter words
-  res = content.match(re);
-  console.log(res);
+   var content = document.getElementById("content").innerText;
+   console.log(content);
+   var re = /\b\w{4}\b/g; // regex to find all 4 letter words
+   res = content.match(re);
+   console.log(res, res.length);
+   l = res.length;
+   for(i = 0; i < l; i++){
+      getData();
+   }
 });
